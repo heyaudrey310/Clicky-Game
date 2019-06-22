5dd6b13d0cb9fd94914976dff078e30a834dc13b
Audrey Agyemang <audreyagyemang310@gmail.com>
2019-06-22 10:45:44 -0400

    Updates

---
+++ precache-manifest.7f9c0ded5b8f7dc97645a0ab1c7dca69.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9077c9e5f0bda90b170f948a5919d162",
+    "url": "/my-app/index.html"
+  },
+  {
+    "revision": "5cb75ae758314793506f",
+    "url": "/my-app/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "2e0129063922e3bb197a",
+    "url": "/my-app/static/js/2.b41502e9.chunk.js"
+  },
+  {
+    "revision": "5cb75ae758314793506f",
+    "url": "/my-app/static/js/main.41e591c4.chunk.js"
+  },
+  {
+    "revision": "9930044c31793394defe",
+    "url": "/my-app/static/js/runtime~main.6019dd84.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/my-app/static/media/logo.5d5d9eef.svg"
+  }
+]);
